b965a7daf76c4bbb137f940f2b6f3dbaf84c4f02
Archit Chauhan <architchauhan910@gmail.com>
2020-12-09 19:06:28 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="import_export",
+    version="0.0.2",
+    author="Archit Chauhan",
+    author_email="architchauhan910@gmail.com",
+    description="Import export bulk",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 2.7", "Python :: 3.6", "Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+)
